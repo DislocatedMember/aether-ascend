50079885fa3318c391da656c44aa42efe7a956a6
Dis Mem <max.dueppe@gmx.net>
2025-07-10 20:26:01 +0200

    removed the deletion of platforms when colliding with the death collider

--- components/game_over/gameover_collider.gd
+++ components/game_over/gameover_collider.gd
@@ -4,5 +4,3 @@ extends Node
 func _on_body_entered(body: Node2D) -> void:
 	if body.name == "character_dummy":
 		StateManager.set_state("gameover")
-	if body.is_in_group("platforms"):
-		body.queue_free()
