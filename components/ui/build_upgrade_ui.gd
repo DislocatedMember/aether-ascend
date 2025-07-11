62e5475e64d945d1815cf49b49a0a9148238c3c5
Dis Mem <max.dueppe@gmx.net>
2025-07-11 22:44:50 +0200

    changed variable name and added currency functionality
    - stat_name is now called stat_name_local
    - on upgrade_pressed available currency is checked before updating stat
    - currency is actually depleted on upgrading

--- components/ui/build_upgrade_ui.gd
+++ components/ui/build_upgrade_ui.gd
@@ -6,6 +6,6 @@ func _ready() -> void:
 	var stat_names = PlayerData.stat_data.get_stat_names()
 	for stat_name in stat_names:
 		var node = upgrade_list_item_scn.instantiate() as stat_view
-		node.stat_name = stat_name
+		node.stat_name_local = stat_name
 		add_child(node)
 			
--- components/ui/upgrade_stat_view.gd
+++ components/ui/upgrade_stat_view.gd
@@ -1,25 +1,30 @@
 class_name stat_view
 extends Node
 
-@export var stat_name: String
+@export var stat_name_local: String
 @export var stat_name_label: Label
 @export var value_label: Label
 @export var cost_label: Label
 @export var upgrade_button: Button
+@export var current_currency_label: Label
 var operation: PlayerData.Operation
 
 func _ready() -> void:
-	update_value(stat_name, PlayerData.get_stat(stat_name))
-	cost_label.text = str(PlayerData.get_upgrade_cost(stat_name))
-	stat_name_label.text = stat_name.capitalize()
+	update_value(stat_name_local, PlayerData.get_stat(stat_name_local))
+	cost_label.text = str(PlayerData.get_upgrade_cost(stat_name_local))
+	stat_name_label.text = stat_name_local.capitalize()
 	PlayerData.player_stat_changed.connect(update_value)	
 
 func _on_upgrade_pressed() -> void:
-	PlayerData.modify_stat(stat_name, PlayerData.get_upgrade_stat_multiplier(stat_name), operation)
-	value_label.text = "%.1f" % [PlayerData.get_stat(stat_name)]
+	var upgrade_cost = PlayerData.get_upgrade_cost(stat_name_local)
+	if PlayerData.get_currency() >= upgrade_cost :
+		PlayerData.decrease_currency(upgrade_cost)
+		print("%s upgrade button pressed" % stat_name_local)
+		PlayerData.modify_stat(stat_name_local, PlayerData.get_upgrade_stat_modifier(stat_name_local), PlayerData.get_upgrade_operation(stat_name_local))
+		value_label.text = "%.1f" % [PlayerData.get_stat(stat_name_local)]
 
 func update_value(stat_name: String, value: Variant):
-	if stat_name == self.stat_name:
+	if stat_name == stat_name_local:
 		value_label.text = "%.1f" % [value]
 
 	
