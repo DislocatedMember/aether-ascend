7f1df8c685fb4a09b9d7e528c2b39fed1299656a
Dis Mem <max.dueppe@gmx.net>
2025-07-12 20:42:20 +0200

    added operation variable to determine operation used to modify stat on upgrade

--- core/upgrade_info.gd
+++ core/upgrade_info.gd
@@ -3,4 +3,5 @@ class_name UpgradeInfo
 
 @export var cost: int
 @export var cost_multiplier: float
-@export var stat_multiplier: float
+@export var stat_modifier: float
+@export var stat_mult_operation: PlayerData.Operation
